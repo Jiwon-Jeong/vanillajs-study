f7aa3f14ef64e4d33d303b337b9dad5d235349db
JIWON <ziwon16@gmail.com>
2020-01-27 23:21:08 +0900

    DOM function if/else, toggle ..

--- study/index.js
+++ study/index.js
@@ -1,22 +1,20 @@
 const title = document.querySelector("#title");
-const BASE_COLOR = "rgb(52, 73, 94)";
-const OTHER_COLOR = "#7f8c8d";
+
+const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-  const currentColor = title.style.color;
-  if (currentColor === BASE_COLOR) {
-    // If the color is same as the initial one
-    title.style.color = OTHER_COLOR;
-  } else {
-    // If the color has changed,
-    // we make it into the initial one again
-    title.style.color = BASE_COLOR;
-  }
+  // const hasClass = title.classList.contains(CLICKED_CLASS);
+  // if (!hasClass) {
+  //   title.classList.add(CLICKED_CLASS);
+  // } else {
+  //   title.classList.remove(CLICKED_CLASS);
+  // }
+
+  title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
-  title.style.color = BASE_COLOR;
+  title.addEventListener("click", handleClick);
 }
 
-title.addEventListener("click", handleClick);
 init();
